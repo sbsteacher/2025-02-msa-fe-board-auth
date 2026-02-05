6b7bd3fcc0e49a71fec891fecfa8e62046671cb4
sbsteacher <id@gmail.com>
2026-02-05 11:06:18 +0900

    signup 작업 중

---
+++ src/views/SignUp.vue
@@ -0,0 +1,35 @@
+<script setup>
+import { reactive } from 'vue';
+
+const state = reactive({
+    signup: {
+        id: '',
+        pw: '',
+        nm: '',
+        gender: 2
+    },
+    checkPw: ''
+});
+
+const doSignUp = async () => {
+    alert('연결됐나?');
+}
+</script>
+
+<template>
+<h3>회원 가입</h3>
+<div><label>아이디: <input type="text" v-model="state.signup.id"></label></div>
+<div><label>비밀번호: <input type="password" v-model="state.signup.pw"></label></div>
+<div><label>확인비밀번호: <input type="password"  v-model="state.checkPw"></label></div>
+<div><label>이름: <input type="text" v-model="state.signup.nm"></label></div>
+<div>
+    성별:
+    <label>여성<input type="radio" value="2" v-model="state.signup.gender"></label>
+    <label>남성<input type="radio" value="1" v-model="state.signup.gender"></label>
+</div>
+<div><button>회원가입</button></div>
+</template>
+
+<style scoped>
+
+</style>
